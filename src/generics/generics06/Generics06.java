b7d7e82c65611c5c3954b9fb28d82f3c9d244c17
Fazıl Demir <fazildemir20@gmail.com>
2023-05-21 17:24:31 +0300

    updated

---
+++ src/generics/generics06/Generics06.java
@@ -0,0 +1,23 @@
+package generics.generics06;
+
+
+public class Generics06 {
+    public static void main(String args[])
+    {
+        GenericsWithObjects<Integer> integerObject = new GenericsWithObjects<Integer>(12);
+        GenericsWithObjects<String> stringObject = new GenericsWithObjects<String>("AdvanceJava");
+        //integerObject = stringObject; // CTE veir Integer data tipine String data tipinde deger atamaya calisildigi icin
+        integerObject.print(); // LINE B
+    }
+}
+class GenericsWithObjects<T>
+{
+    T obj;
+    GenericsWithObjects(T obj)
+    {
+        this.obj = obj;
+    }
+    void print()
+    {
+        System.out.println(obj);
+    }
